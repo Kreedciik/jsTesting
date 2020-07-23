140c5db5258a2fa02d74aa09ab3a0ca89aec0bcb
kreedciik <example@gmail.com>
2020-07-23 10:59:14 +0500

    Create Income calculate in js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 'use strict'
 
+let askMoney = prompt("Ваш бюджет на месяц?", ''),
+    typeDate = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+
+    money: askMoney,
+    time: typeDate,
+    expenses: {},
+    optionalExpense: {},
+    income: [],
+    savings: false
+
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+    
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
+    
+    alert(appData.money / 30);
+
